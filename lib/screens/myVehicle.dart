9865df7414a3995246764b759c67c06763af39c7
James Alberto <c14190138@john.petra.ac.id>
2023-05-11 00:56:35 +0700

    - add +62 on phone number on profile page - add is empty on my vehicle and ridelist - add back button on tnc

--- lib/screens/myVehicle.dart
+++ lib/screens/myVehicle.dart
@@ -53,216 +53,220 @@ class _MyVehicleState extends State<MyVehicle> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.grey[50],
-        elevation: 0,
-        toolbarHeight: 100,
-        leading: Container(
-          margin: EdgeInsets.only(top: 52),
-          child: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.black),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+      body: Stack(
+        children: [
+          Positioned(
+            //topright
+            top: 0,
+            right: 0,
+            child: Image(
+              image: AssetImage('assets/backgroundCircle/backgroundCircle3.png'),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.only(top: 12, left: 28, right: 28),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              BorderedText(
-                child: Text(
-                  "Kendaraanku",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32,
-                  ),
-                ),
-                strokeWidth: 3.0,
-                strokeColor: Colors.black,
-              ),
-              SizedBox(height: 30),
-              Container(
-                width: 240,
-                height: 40,
-                margin: EdgeInsets.zero,
-                child: ElevatedButton(
+          Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  icon: Icon(Icons.arrow_back, color: Colors.black),
                   onPressed: () {
-                    Navigator.of(context).pushNamed('/addVehicle');
+                    Navigator.pop(context);
                   },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.add_circle,
-                        color: Colors.black,
-                        size: 30,
-                      ),
-                      SizedBox(width: 10),
-                      NunutText(
-                        title: "Tambah Kendaraan",
-                        color: Colors.black,
-                        size: 16,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ],
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18),
-                      side: BorderSide(
-                        color: Colors.black,
-                        width: 2.0,
-                      ),
+                ),
+                SizedBox(height: 20),
+                NunutText(title: "Kendaraanku", size: 32, isTitle: true),
+                SizedBox(height: 30),
+                Container(
+                  width: 240,
+                  height: 40,
+                  margin: EdgeInsets.zero,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/addVehicle');
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.add_circle,
+                          color: Colors.black,
+                          size: 30,
+                        ),
+                        SizedBox(width: 10),
+                        NunutText(
+                          title: "Tambah Kendaraan",
+                          color: Colors.black,
+                          size: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ],
                     ),
-                    elevation: 0.0,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                        side: BorderSide(
+                          color: Colors.black,
+                          width: 2.0,
+                        ),
+                      ),
+                      elevation: 0.0,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 30),
-              isDataLoaded
-                  ? vehicleList(vehicles)
-                  : Center(
-                      child: CircularProgressIndicator(
-                        color: nunutPrimaryColor,
-                        strokeWidth: 2,
+                SizedBox(height: 30),
+                isDataLoaded
+                    ? vehicles.length == 0
+                        ? Container(
+                            margin: EdgeInsets.only(top: 70),
+                            child: Center(
+                              child: NunutText(title: "Data tidak ditemukan", size: 18, color: Colors.grey),
+                            ),
+                          )
+                        : vehicleList(vehicles)
+                    : Center(
+                        heightFactor: 2,
+                        child: CircularProgressIndicator(
+                          color: nunutPrimaryColor,
+                          strokeWidth: 2,
+                        ),
                       ),
-                    ),
-              SizedBox(height: 20),
-            ],
+                SizedBox(height: 20),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
 
   Widget vehicleList(List<Vehicle2> vehicles) {
     return ListView.separated(
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.only(top: 20, bottom: 0, left: 20, right: 20),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: Container(
-                              child: Image.asset(
-                                "assets/car.png",
-                                width: 70,
-                                height: 70,
-                              ),
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 20, bottom: 0, left: 20, right: 20),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                            child: Image.asset(
+                              "assets/car.png",
+                              width: 70,
+                              height: 70,
                             ),
                           ),
-                          Column(
-                            children: [
-                              NunutText(
-                                title: vehicles[index].transportationType ?? "",
-                                color: Colors.black,
-                                size: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              NunutText(
-                                title: vehicles[index].licensePlate ?? "",
-                                color: Colors.black,
-                                size: 16,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                      SizedBox(height: 20),
-                    ],
+                        ),
+                        Column(
+                          children: [
+                            NunutText(
+                              title: vehicles[index].transportationType ?? "",
+                              color: Colors.black,
+                              size: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            NunutText(
+                              title: vehicles[index].licensePlate ?? "",
+                              color: Colors.black,
+                              size: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
                   ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                    ),
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 1.0,
-                    ),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1.0,
                   ),
                 ),
-                vehicles[index].isMain!
-                    ? Container(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        alignment: Alignment.center,
-                        width: double.infinity,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            NunutText(
-                              title: "KENDARAAN UTAMA",
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                            SizedBox(width: 10),
-                            Icon(
-                              Icons.check_circle,
-                              color: Colors.white,
-                            ),
-                          ],
+              ),
+              vehicles[index].isMain!
+                  ? Container(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      alignment: Alignment.center,
+                      width: double.infinity,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          NunutText(
+                            title: "KENDARAAN UTAMA",
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.check_circle,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
                         ),
-                        decoration: BoxDecoration(
+                        border: Border.all(
                           color: Colors.black,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(20),
-                            bottomRight: Radius.circular(20),
-                          ),
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 1.0,
-                          ),
-                        ),
-                      )
-                    : Container(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        alignment: Alignment.center,
-                        width: double.infinity,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            NunutText(
-                              title: "",
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                            SizedBox(width: 10),
-                          ],
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(20),
-                            bottomRight: Radius.circular(20),
-                          ),
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 1.0,
-                          ),
+                          width: 1.0,
                         ),
                       ),
-              ],
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return SizedBox(height: 12);
-        },
-        itemCount: vehicles.length);
+                    )
+                  : Container(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      alignment: Alignment.center,
+                      width: double.infinity,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          NunutText(
+                            title: "",
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 10),
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        ),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 1.0,
+                        ),
+                      ),
+                    ),
+            ],
+          ),
+        );
+      },
+      separatorBuilder: (context, index) {
+        return SizedBox(height: 12);
+      },
+      itemCount: vehicles.length,
+    );
   }
 }
