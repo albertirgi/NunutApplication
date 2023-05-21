5c8812d7737551631d0210290c79b53880295ae8
James Alberto <c14190138@john.petra.ac.id>
2023-05-21 13:13:07 +0700

    add check if there is new version -> user must update first to continue using app

--- lib/screens/splashScreen.dart
+++ lib/screens/splashScreen.dart
@@ -2,11 +2,13 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:new_version/new_version.dart';
 import 'package:nunut_application/configuration.dart';
 import 'package:nunut_application/models/mresult.dart';
 import 'package:nunut_application/models/muser.dart';
 import 'package:nunut_application/resources/userApi.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:http/http.dart' as http;
 
 class SplashScreen extends StatefulWidget {
@@ -18,16 +20,32 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   bool goToMain = false;
+  bool newVersion = false;
   @override
   void initState() {
     checkToken();
-    Timer(Duration(seconds: 3), () {
-      goToMain ? Navigator.pushReplacementNamed(context, '/main') : Navigator.pushReplacementNamed(context, '/login');
-    });
+    newVersion
+        ? Timer(Duration(seconds: 3), () {
+            goToMain ? Navigator.pushReplacementNamed(context, '/main') : Navigator.pushReplacementNamed(context, '/login');
+          })
+        : null;
     super.initState();
   }
 
   void checkToken() async {
+    final status = await NewVersion().getVersionStatus();
+    if (status!.canUpdate) {
+      newVersion = true;
+      NewVersion().showUpdateDialog(
+        context: context,
+        versionStatus: status,
+        dialogTitle: 'Versi baru tersedia',
+        dialogText: 'Silahkan update versi terbaru untuk lanjut menggunakan aplikasi',
+        allowDismissal: false,
+        updateButtonText: 'Update sekarang',
+      );
+      return;
+    }
     SharedPreferences prefs = await SharedPreferences.getInstance();
     if (prefs.getString("token") != null) {
       var url = Uri.parse(config.baseUrl + '/check-token/${prefs.getString("token")}');
